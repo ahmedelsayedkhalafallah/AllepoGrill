9bb64f7afe00ce234bafe06c8b268f32b496b389
ahmedelsayedkhalafallah <ahmedelsayedkhalafallah@yahoo.com>
2019-03-18 20:40:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ahmed/allepogrill/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.ahmed.allepogrill;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbarTop = findViewById(R.id.toolbar_top);
+        TextView mTitle = toolbarTop.findViewById(R.id.toolbar_title);
+    }
+}
